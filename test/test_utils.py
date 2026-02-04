bf9cd82c54efe0b49a575d335787d29eadde48d2
Andreas Bresser <andreas.bresser@dfki.de>
2026-02-04 16:40:07 +0100

    test saving to (temp)file

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,14 +1,15 @@
 """Test loading/saving data."""
 from pathlib import Path
+import tempfile
 
 from video_stream_data import Codec, Encoding, StreamInfo, StreamValue
-from video_stream_data.utils import load
+from video_stream_data.utils import load, save
 
 BASE_PATH = Path(__file__).absolute().parent
 LOCAL_VIDEO_DEVICE = BASE_PATH / 'data' / 'local_video_device.yml'
 
 
-def test_stream_info():
+def test_load_stream_info():
     """Test loading data from yaml."""
     stream_info = load(LOCAL_VIDEO_DEVICE)
     assert len(stream_info) == 1
@@ -26,3 +27,20 @@ def test_stream_info():
             power_line_freq = True
             break
     assert power_line_freq
+
+
+def test_save_stream_info():
+    """Test saving data to temp file."""
+    stream_info = load(LOCAL_VIDEO_DEVICE)
+    tmp = tempfile.NamedTemporaryFile()
+    save(stream_info, tmp.name)
+
+    # check file by string compare
+    saved_data = ''
+    with open(tmp.name, 'r') as data:
+        saved_data = data.read()
+    assert 'Dummy Camera' in saved_data
+
+    # check full structure by reloading
+    reloaded_stream_info = load(tmp.name)
+    assert reloaded_stream_info == stream_info
--- video_stream_data/utils.py
+++ video_stream_data/utils.py
@@ -3,12 +3,19 @@ from pathlib import Path
 
 import yaml
 
-from . import StreamInfo
+from . import stream_as_dict, StreamInfo
 
 
-def load(filename: Path | str) -> None:
-    """Load the yaml file and return it as StreamInfo."""
+def load(filename: Path | str) -> None | list[StreamInfo]:
+    """Load the yaml file and return it as list of StreamInfo."""
     with open(filename, 'r') as data:
         yaml_data = yaml.safe_load(data)
         return [StreamInfo(**stream) for stream in yaml_data]
     return None
+
+
+def save(data: list[StreamInfo], filename: Path | str) -> None:
+    """Save list of StreamInfo to yaml file."""
+    yaml_data = [stream_as_dict(s) for s in data]
+    with open(filename, 'w') as data:
+        data.write(yaml.safe_dump(yaml_data))
